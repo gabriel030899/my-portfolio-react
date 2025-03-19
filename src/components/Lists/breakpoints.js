a325e9530c102040568de347d7a7302b39b28aa6
gabriel030899git config --global user.email gabriel030899@gmail.com <gabrieldoissteam@hotmail.com>
2025-03-19 15:53:54 -0300

    created responsivity for home screen

---
+++ src/components/Lists/breakpoints.js
@@ -0,0 +1,13 @@
+const size = {
+    sm: '600px',
+    md: '800px',
+    bg: '1030px'
+}
+
+const breakpoints = {
+    sm: `(max-width: ${size.sm})`,
+    md: `(max-width: ${size.md})`,
+    bg: `(max-width: ${size.bg})`
+}
+
+export default breakpoints
